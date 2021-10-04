5a7127f747b155f76dce967e5d000ad34942958c
Hugh Warden <hugh.warden@outlook.com>
2021-10-04 15:42:49 +0100

    Finishing incidence matrix function

--- R/create_matrices.R
+++ R/create_matrices.R
@@ -4,6 +4,36 @@ incidence_matrix <- function(hype, augment_oriented = TRUE) {
   }
 
   if (hype$get_oriented()) {
+    elist <- hype$get_elist()
+    numv <- hype$get_numv()
+    nume <- length(elist)
+
+    inc_mat <- list(
+      matrix(0, nrow = numv, ncol = nume),
+      matrix(0, nrow = numv, ncol = nume)
+    )
+
+    if (hype$get_directed()) {
+      names(inc_mat) <- c("from", "to")
+    }
+
+    rownames(inc_mat[[1]]) <- hype$get_vnames()
+    colnames(inc_mat[[1]]) <- hype$get_enames()
+    rownames(inc_mat[[2]]) <- hype$get_vnames()
+    colnames(inc_mat[[2]]) <- hype$get_enames()
+
+    for (i in 1:nume) {
+      inc_mat[[1]][elist[[i]][[1]], i] <- 1
+      inc_mat[[2]][elist[[i]][[2]], i] <- 1
+    }
+
+    if (hype$get_oriented() & !hype$get_directed() & augment_oriented) {
+      temp <- inc_mat[[1]]
+      inc_mat[[1]] <- cbind(inc_mat[[1]], inc_mat[[2]])
+      inc_mat[[2]] <- cbind(inc_mat[[2]], temp)
+    }
+
+    return(inc_mat)
 
   } else {
     elist <- hype$get_elist()
