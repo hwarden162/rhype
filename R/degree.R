25febd3258ca2f33d169552166989cfc2ec44f4d
Hugh Warden <hugh.warden@outlook.com>
2021-10-08 01:56:53 +0100

    Adding roxygen commenting for degree function

--- R/degree.R
+++ R/degree.R
@@ -1,3 +1,37 @@
+#' Find the Degree of Vertices in a Hypergraph
+#'
+#' The degree of a vertex is a way of expressing how many connections there
+#' are from a vertex to the rest of the hypergraph. The current version of
+#' hypR has three methods for computing degree.
+#'
+#' `"vertex"` counts the number of ways it is possible to move to another
+#' vertex. If there are multiple hyperedges connecting two vertices, then each
+#' of these hyperedges will be counted as a new way to move between these two
+#' vertices. For weighted hypergraphs or hypergraphs with real coefficients,
+#' the strength of connection between two vertices is a functions of the weights
+#' and real coefficients.
+#'
+#' `"vertex_simple"` just counts the number of vertices it is possible to reach
+#' in one step from the given vertex, no matter how many hyperedges connect them.
+#'
+#' `"hyperedge"` represents the strength with which a vertex connects with
+#' itself through the hyperedges it is a member of. This is taken from the work
+#' of Jurgen Jost and Raffaella Mulas
+#' <doi:https://doi.org/10.1016/j.aim.2019.05.025>. For unweighted hypergraphs
+#' without real coefficients this is equivalent to `"hyperedge_simple"`.
+#'
+#' `"hyperedge_simple"` just counts the number of hyperedges a vertex is a
+#' member of.
+#'
+#' @param hype A hypergraph object
+#' @param method The method for calculating degree. Out of `"vertex"`,
+#' `"vertex_simple"`, `"hyperedge"` and `"hyperedge_simple"`
+#'
+#' @return A vector representing the degree of each vertex with respect to the
+#' given method.
+#' @export
+#'
+#' @examples
 degree <- function(hype, method = "vertex") {
   if (hype$get_oriented()) {
     stop("\n \u2716 Degree not yet supported for oriented hypergraphs")
@@ -14,5 +48,9 @@ degree <- function(hype, method = "vertex") {
   } else if (method == "hyperedge") {
     adj_mat <- adjacency_matrix(hype, normalise = FALSE, self_adj = TRUE)
     return(diag(adj_mat))
+  } else if (method == "hyperedge_simple") {
+    inc_mat <- incidence_matrix(hype)
+    inc_mat <- matrix(as.numeric(inc_mat != 0), nrow = dim(inc_mat)[1])
+    return(apply(inc_mat, 1, sum))
   }
 }
