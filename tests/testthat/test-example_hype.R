b5eb75eca61d0bd33ab440b7f8b1fa1c54a65554
Hugh Warden <hugh.warden@outlook.com>
2022-08-24 01:05:38 +0100

    Adding example function test

---
+++ tests/testthat/test-example_hype.R
@@ -0,0 +1,37 @@
+test_that("hypergraph example works", {
+
+  numv = 4
+
+  elist <- list(
+    c(1, 2, 3),
+    c(2, 3, 4)
+  )
+
+  vnames <- paste0("v", 1:4)
+  enames <- paste0("h", 1:2)
+  names(elist) <- enames
+
+  vweights <- NULL
+  eweights <- NULL
+
+  real_coef <- FALSE
+  inc_mat <- NULL
+
+  expect_equal(
+    example_hype(),
+    Hypergraph$new(
+      numv = numv,
+      elist = elist,
+      vnames = vnames,
+      vweights = vweights,
+      enames = enames,
+      eweights = eweights,
+      weighted = FALSE,
+      oriented = FALSE,
+      directed = FALSE,
+      real_coef = real_coef,
+      inc_mat = inc_mat
+    )
+  )
+
+})
